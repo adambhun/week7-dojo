cd6bbcc73917408f0aa008b7ad3147665842727f
Adam Bene <youremail@example.com>
2019-03-04 09:56:53 +0100

    solution 2 redone

--- kata2/kata2.ts
+++ kata2/kata2.ts
@@ -7,13 +7,13 @@ export function chop (array:number[], number:number) {
 }
 
 export function chop2 (array:number[], number:number) {
-  let ret: number = 0;
   for (let index = 0; index < array.length; index++) {
     if (array[index] === number) {
-      ret = index;
+      return index;
     }
   }
-  return ret;
 }
+
+
 
 console.log(chop2(arr, 9));
