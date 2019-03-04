ce7d54bb6f930885f7ca9f02a40c86e44625fa2f
Adam Bene <youremail@example.com>
2019-03-04 09:44:10 +0100

    setup

---
+++ kata2.ts
@@ -0,0 +1,9 @@
+'use strict';
+
+export const arr: number[] = [0, 3, 4, 8, 1, 9, 18];
+
+export function chop (params:number[]) {
+  
+}
+
+console.log(chop(arr));
---
+++ test1.ts
@@ -0,0 +1,6 @@
+'use strict';
+
+import { chop, arr } from './kata2';
+let test = require('tape');
+
+
