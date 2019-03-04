539a84ec981dc270f5216b9b5cbaacadb879900d
Adam Bene <youremail@example.com>
2019-03-04 09:55:42 +0100

    second solution

--- kata2/kata2.ts
+++ kata2/kata2.ts
@@ -6,4 +6,14 @@ export function chop (array:number[], number:number) {
   return array.indexOf(number);
 }
 
-console.log(chop(arr, 9));
+export function chop2 (array:number[], number:number) {
+  let ret: number = 0;
+  for (let index = 0; index < array.length; index++) {
+    if (array[index] === number) {
+      ret = index;
+    }
+  }
+  return ret;
+}
+
+console.log(chop2(arr, 9));
